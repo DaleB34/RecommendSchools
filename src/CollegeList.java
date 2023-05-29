8879b422ba695690b66dc79c25e61bec17296aaf
DaleB34 <117684559+DaleB34@users.noreply.github.com>
2023-05-29 18:31:14 +0000

    Added filter method

--- src/CollegeList.java
+++ src/CollegeList.java
@@ -7,8 +7,19 @@ public class CollegeList
     private int count = 0;
     private ArrayList<College> recColleges;
 
-    public void addCollege()
+    public void addCollege(College c)
     {
-        recColleges.add()
+        colleges[count] = c;
+        count++;
+    }
+
+    public College filter(College c, int pPrice, int pRate, double pGpa, int pRating)
+    {
+        int conditions = 0; //count number of conditions met, 3+/4 means it goes into list
+
+        if(c.totalPrice() >= pPrice)
+            conditions++;
+        
+        if(c.)
     }
 }
