13c987d3ba3b8db40e29019ff08389cd3cc1293f
91112021 <dalebolido@gmail.com>
2023-06-02 12:36:59 -0700

    added mvc and rcc

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         String[] floppaMajors = {"Flopping", "Idk"};
-        PrivateUni floppaU = new PrivateUni("FloppaU", "FloppaLand", 12345.67, 1000.0
+        PrivateUni floppaU = new PrivateUni("Floppa University", "FloppaLand", 12345.67, 1000.0
                                             , 0.2, 0.0, 0.0, 10000
                                             , 999, 0.0, 0.0, floppaMajors);
         //floppaU on top
@@ -14,9 +14,18 @@ public class Main {
         UC sanDiego = new UC("UC San Diego", "San Diego, California", 14700.0, 34.2, 3.4
                             ,44454.21, 1101.34, 3900, 7,17039.90);
 
-        UC la = new UC("UCLA", "Los Angeles, California", 13401.53, 10.77, 3.4
+        UC la = new UC("UC Los Angeles", "Los Angeles, California", 13401.53, 10.77, 3.4
                         ,43473.23, 1574.59, 125, 9, 15374.76);
         CalState usd = new CalState("San Diego State University", "San Diego, California", 8136.42, 46.9
                                     ,3.0, 20016.81, 1079.43, 86, 10, 30.9);
+        CalState usb = new CalState("California State University San Bernardino", "San Bernardino, California", 7213.84,
+                            91.4,3.34, 19093.76, 1272.83, 70, 6,  49.81);
+        College mvc = new College("Moreno Valley College", "Moreno Valley, California", 1420.22, 100.0,
+                                    3.0, 10120.99, 42.90, 22, 3);
+        College rcc = new College ("Riverside City College", "Riverside, California", 1432.89, 100.0,
+                                    2.0, 10120.98, 1440.0, 40, 5);
+
+
+
     }
 }
